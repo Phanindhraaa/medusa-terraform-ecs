1605617506d62416518daeb01d0ccc0418766d5f
Phanindhraaa <suraphanindhra@gmail.com>
2025-05-31 06:28:48 +0000

    Add VPC with DNS support and tagging for ECS public networking

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,14 @@
-provider "aws" {
-  region = var.region
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "medusa-vpc"
+  }
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
+data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "public" {
   count                   = 2
@@ -12,14 +16,27 @@ resource "aws_subnet" "public" {
   cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                     = "medusa-public-subnet-${count.index}"
+    "kubernetes.io/role/elb" = "1"
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "medusa-igw"
+  }
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "medusa-public-rt"
+  }
 }
 
 resource "aws_route" "internet_access" {
@@ -33,4 +50,3 @@ resource "aws_route_table_association" "public_assoc" {
   subnet_id      = element(aws_subnet.public.*.id, count.index)
   route_table_id = aws_route_table.public.id
 }
-
