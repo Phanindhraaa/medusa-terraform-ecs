718a8010ba3c3c2ce0d911aebe97e847e3f9e009
Phanindhraaa <suraphanindhra@gmail.com>
2025-05-31 10:18:23 +0000

    Update RDS engine_version to 15.13

--- rds.tf
+++ rds.tf
@@ -1,31 +1,7 @@
-resource "aws_security_group" "db_sg" {
-  name        = "medusa-db-sg"
-  description = "Security group for Medusa RDS instance"
-  vpc_id      = aws_vpc.main.id # Make sure this references your VPC resource correctly
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # You can restrict this to your app servers' SG or CIDR for security
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "medusa-db-sg"
-  }
-}
-
 resource "aws_db_instance" "medusa_rds" {
   allocated_storage      = var.db_allocated_storage
   engine                 = "postgres"
-  engine_version         = "15.3"
+  engine_version         = "15.13" # or "17.3"
   instance_class         = var.db_instance_class
   db_name                = var.db_name
   username               = var.db_username
@@ -33,9 +9,6 @@ resource "aws_db_instance" "medusa_rds" {
   publicly_accessible    = true
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.db_sg.id]
-
-  # Optionally add subnet group if you have it declared:
-  # db_subnet_group_name = aws_db_subnet_group.medusa_subnet_group.name
 
   tags = {
     Name = "medusa-rds-instance"
