d581af10d34dd03ce7881697f789738a35e68990
Phanindhraaa <suraphanindhra@gmail.com>
2025-05-31 10:24:19 +0000

    Add RDS security group definition

--- rds.tf
+++ rds.tf
@@ -14,3 +14,27 @@ resource "aws_db_instance" "medusa_rds" {
     Name = "medusa-rds-instance"
   }
 }
+
+resource "aws_security_group" "db_sg" {
+  name        = "medusa-db-sg"
+  description = "Security group for Medusa RDS instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Consider restricting this for security
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "medusa-db-sg"
+  }
+}
