f528aa6bf382360cd1777e6b3146ce94c940a287
Phanindhraaa <suraphanindhra@gmail.com>
2025-05-31 13:44:28 +0000

    Fix formatting for Terraform files

--- rds_subnet_group.tf
+++ rds_subnet_group.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "medusa_subnet_group" {
-  name       = "medusa-db-subnet-group"
+  name = "medusa-db-subnet-group"
   subnet_ids = [
     aws_subnet.public[0].id,
     aws_subnet.public[1].id
